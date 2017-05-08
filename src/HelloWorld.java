31ad5c3c0176e51d2245c4938350e33418a4d810
Carlos <c.soto93@hotmail.com>
2017-05-08 14:47:31 -0500

    exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,79 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+
+//        int myFavoriteNumber = 19;
+//        System.out.println(myFavoriteNumber);
+
+//        String myString = "My String variable";
+//        System.out.println(myString);
+
+//        char myString = 'A';
+//        System.out.println(myString);
+
+//        float myString = (int) 3.14159;
+//        System.out.println(myString);
+
+//        long myNumber;
+//        System.out.println(myNumber);
+
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        float myNumber = (float) 3.14;
+//        System.out.println(myNumber);
+
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class = 5;
+//        System.out.println(class);
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
