571ffa96d156020275ea5f29a68ec287a502928e
Carlos <c.soto93@hotmail.com>
2017-05-14 14:32:14 -0500

    Updated factorial exercise to check if number is valid befor performing method.

--- src/Exercises/MethodsExercises.java
+++ src/Exercises/MethodsExercises.java
@@ -8,19 +8,19 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 ////        for 1)
-//        int x = 45;
-//        int y = 12;
+//        int x = 2;
+//        int y = 3;
 //
-//        System.out.println(addition(x, y));
-//        System.out.println(subtraction(x, y));
-//        System.out.println(multiplication(x, y));
+//        System.out.println("Addition: " + addition(x, y));
+//        System.out.println("Subtraction: " + subtraction(x, y));
+//        System.out.println("Multiplication: " +multiplication(x, y));
 //        if (y != 0) {
-//            System.out.println(division(x, y));
+//            System.out.println("Division: " + division(x, y));
 //        } else {
 //            System.out.println("You can not divide by 0");
 //        }
 //        if (y != 0) {
-//            System.out.println(modulus(x, y));
+//            System.out.println("Modulus: " + modulus(x, y));
 //        } else {
 //            System.out.println("You can not modulus by 0");
 //        }
@@ -32,17 +32,22 @@ public class MethodsExercises {
 
 
 ////        for 3)
-//        String anotherFactorial = "y";
-//        do {
-//            if (anotherFactorial.equalsIgnoreCase("y")) {
-//                System.out.println("(Calculate Factors)");
-//                System.out.println("Enter a number between 1 and 65.");
-//                int userNumber = input.nextInt();
-//                System.out.println("Factorial = " + calculateFactorial(userNumber));
-//            }
-//            System.out.println("Calculate another Factorial? y/n");
-//            anotherFactorial = input.next();
-//        } while (anotherFactorial.equalsIgnoreCase("y"));
+        String anotherFactorial = "y";
+        do {
+            if (anotherFactorial.equalsIgnoreCase("y")) {
+                System.out.println("(Calculate Factors)");
+                System.out.println("Enter a number between 1 and 65.");
+                int userNumber = input.nextInt();
+                if (1 <= userNumber && userNumber <= 65) {
+                    System.out.println("Factorial = " + calculateFactorial(userNumber));
+                } else {
+                    System.out.println("Not a valid number");
+
+                }
+            }
+            System.out.println("Calculate another Factorial? y/n");
+            anotherFactorial = input.next();
+        } while (anotherFactorial.equalsIgnoreCase("y"));
 
 
 ////        for 4)
@@ -62,25 +67,25 @@ public class MethodsExercises {
 
 
 ////        for 5)
-        System.out.println("Lets play Higher Lower. y/n");
-        int gameNumber = (int) (Math.random() * 100 + 1);
-        String wantToPlay = input.next();
-        if (wantToPlay.equalsIgnoreCase("y")) {
-            int userGuess = 0;
-            int guessesLeft = 3;
-            do {
-                System.out.println("Whats your guess? " + guessesLeft + " guesses left");
-                userGuess = input.nextInt();
-                System.out.println(playHigherLower(gameNumber, userGuess, guessesLeft));
-                guessesLeft--;
-                if (guessesLeft == 0) {
-                    userGuess = gameNumber;
-                }
-
-
-            } while (userGuess != gameNumber);
-            System.out.println("The number was " + gameNumber);
-        }
+//        System.out.println("Lets play Higher Lower. y/n");
+//        int gameNumber = (int) (Math.random() * 100 + 1);
+//        String wantToPlay = input.next();
+//        if (wantToPlay.equalsIgnoreCase("y")) {
+//            int userGuess = 0;
+//            int guessesLeft = 3;
+//            do {
+//                System.out.println("Whats your guess? " + guessesLeft + " guesses left");
+//                userGuess = input.nextInt();
+//                System.out.println(playHigherLower(gameNumber, userGuess, guessesLeft));
+//                guessesLeft--;
+//                if (guessesLeft == 0) {
+//                    userGuess = gameNumber;
+//                }
+//
+//
+//            } while (userGuess != gameNumber);
+//            System.out.println("The number was " + gameNumber);
+//        }
 
 
     }
@@ -105,18 +110,18 @@ public class MethodsExercises {
 //        return inputOne + multiplication(inputOne, inputTwo - 1);
 //        }
 //
-//        //answer with a loop
-//        int answer = 0;
-//        for (int i = 1; i <= inputTwo; i++) {
-//            answer += inputOne;
-//        }
-//        return answer;
-//
-//        //answer with *
-//        int answer = inputOne * inputTwo;
-//        return answer;
+////        //answer with a loop
+////        int answer = 0;
+////        for (int i = 1; i <= inputTwo; i++) {
+////            answer += inputOne;
+////        }
+////        return answer;
+////
+////        //answer with *
+////        int answer = inputOne * inputTwo;
+////        return answer;
 //    }
-
+//
 //    public static int division(int inputOne, int inputTwo) {
 //        int answer = inputOne / inputTwo;
 //        return answer;
@@ -142,20 +147,15 @@ public class MethodsExercises {
 
 
     ////    for 3)
-//    public static long calculateFactorial(int userNumber) {
-//        long factorial = userNumber;
-//        if (1 <= userNumber && userNumber <= 65) {
-//            ////Finding Factorial with recursion
-//            if (userNumber == 1) {
-//                return factorial *= userNumber;
-//            } else {
-//                return factorial * calculateFactorial(userNumber - 1);
-//            }
-//        } else {
-//            System.out.println("Not a valid number");
-//            return calculateFactorial(userNumber);
-//        }
-//    }
+    public static long calculateFactorial(int userNumber) {
+        long factorial = userNumber;
+        ////Finding Factorial with recursion
+        if (userNumber == 1) {
+            return factorial *= userNumber;
+        } else {
+            return factorial * calculateFactorial(userNumber - 1);
+        }
+    }
 
 
     ////    for 4)
@@ -168,20 +168,20 @@ public class MethodsExercises {
 
     //////    for 5)
 
-    public static String playHigherLower(int gameNumber, int userGuess, int guessesLeft) {
-        guessesLeft--;
-        if (guessesLeft > 0) {
-            if (userGuess < gameNumber) {
-                return ("HIGHER");
-            } else if (userGuess > gameNumber) {
-                return ("LOWER");
-            } else {
-                return ("GOOD GUESS!");
-            }
-
-        } else {
-            return ("Game Over");
-        }
-    }
+//    public static String playHigherLower(int gameNumber, int userGuess, int guessesLeft) {
+//        guessesLeft--;
+//        if (guessesLeft > 0) {
+//            if (userGuess < gameNumber) {
+//                return ("HIGHER");
+//            } else if (userGuess > gameNumber) {
+//                return ("LOWER");
+//            } else {
+//                return ("GOOD GUESS!");
+//            }
+//
+//        } else {
+//            return ("Game Over");
+//        }
+//    }
 
 }
